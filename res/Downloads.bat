b69336bf14ea7cb8a82678cf9e0358be42f6c344
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-16 19:51:39 +0300

    test

--- res/Downloads.bat
+++ res/Downloads.bat
@@ -1,10 +1,10 @@
 @echo off
 curl -sLo setup.py https://github.com/noob-lol/a2/raw/main/res/setup.py
-curl -sLo Avica_setup.exe https://download.avica.link/downloader/Avica_setup.exe
+curl -sLo Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe"
 curl -sLo show.bat https://github.com/noob-lol/a2/raw/main/res/show.bat
 curl -sLo loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
 curl -sLo C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-701.exe
-curl -sLo C:\Users\Public\Desktop\VMQuickConfig.exe https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 @REM curl -sLo tesseract-setup.exe https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe
 @REM if exist tesseract-setup.exe (
 @REM     tesseract-setup.exe /SILENT
