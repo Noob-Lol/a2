fc8b8cdead46375092db1353756802991322f3ef
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-16 20:32:02 +0300

    hmmm
    still no tesseract

--- res/Downloads.bat
+++ res/Downloads.bat
@@ -10,8 +10,9 @@ curl -sLo C:\Users\Public\Desktop\VMQuickConfig.exe "https://github.com/chieunha
 curl -sLo tesseract-setup.exe "https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe"
 start /wait tesseract-setup.exe /S
 del tesseract-setup.exe
-setx PATH "%PATH%;C:\Program Files\Tesseract-OCR"
+powershell -Command "[Environment]::SetEnvironmentVariable('Path', $env:Path + ';C:\Program Files\Tesseract-OCR', [EnvironmentVariableTarget]::Machine)"
 set PATH=%PATH%
+echo Tesseract was installed
 pip install requests pyautogui pytesseract --quiet
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
