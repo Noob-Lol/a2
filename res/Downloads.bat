604345354031ea69eba9bc2565a35175f5cd0214
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-16 20:26:10 +0300

    bruh no tesseract

--- res/Downloads.bat
+++ res/Downloads.bat
@@ -1,21 +1,17 @@
 @echo off
+SETLOCAL
+echo Downloading...
 curl -sLo setup.py https://github.com/noob-lol/a2/raw/main/res/setup.py
 curl -sLo Avica_setup.exe "https://download.avica.link/downloader/Avica_setup.exe"
 curl -sLo show.bat https://github.com/noob-lol/a2/raw/main/res/show.bat
 curl -sLo loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
 curl -sLo C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-701.exe
-echo this is done
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
-echo downloading tesseract
+curl -sLo C:\Users\Public\Desktop\VMQuickConfig.exe "https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe"
 curl -sLo tesseract-setup.exe "https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe"
-if exist tesseract-setup.exe (
-    echo installing tesseract
-    tesseract-setup.exe /S
-    setx /M PATH "%PATH%;C:\Program Files\Tesseract-OCR"
-    echo Tesseract was installed and added to PATH.
-) else (
-    echo Failed to download Tesseract OCR setup.
-)
+start /wait tesseract-setup.exe /S
+del tesseract-setup.exe
+setx PATH "%PATH%;C:\Program Files\Tesseract-OCR"
+set PATH=%PATH%
 pip install requests pyautogui pytesseract --quiet
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
@@ -24,3 +20,4 @@ del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
 net user runneradmin NoobPass1
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "Avica_setup.exe"
+ENDLOCAL
