29409bfb7c8f4ec6e9200b8d9bfbf8ef59ef5880
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-16 19:25:08 +0300

    temp remove tesseract

--- res/Downloads.bat
+++ res/Downloads.bat
@@ -5,14 +5,14 @@ curl -sLo show.bat https://github.com/noob-lol/a2/raw/main/res/show.bat
 curl -sLo loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
 curl -sLo C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-701.exe
 curl -sLo C:\Users\Public\Desktop\VMQuickConfig.exe https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe
-curl -sLo tesseract-setup.exe https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe
-if exist tesseract-setup.exe (
-    tesseract-setup.exe /SILENT
-    setx /M PATH "%PATH%;C:\Program Files\Tesseract-OCR"
-    echo Tesseract was installed and added to PATH.
-) else (
-    echo Failed to download Tesseract OCR setup.
-)
+@REM curl -sLo tesseract-setup.exe https://github.com/UB-Mannheim/tesseract/releases/download/v5.4.0.20240606/tesseract-ocr-w64-setup-5.4.0.20240606.exe
+@REM if exist tesseract-setup.exe (
+@REM     tesseract-setup.exe /SILENT
+@REM     setx /M PATH "%PATH%;C:\Program Files\Tesseract-OCR"
+@REM     echo Tesseract was installed and added to PATH.
+@REM ) else (
+@REM     echo Failed to download Tesseract OCR setup.
+@REM )
 pip install requests pyautogui pytesseract --quiet
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
