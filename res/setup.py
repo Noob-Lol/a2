923ff750fc56e8afb275aaaeb0a3dd0390ca81b6
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-18 20:44:37 +0300

    wait until ready

--- res/setup.py
+++ res/setup.py
@@ -1,18 +1,21 @@
-import os, time, requests, subprocess
+import os, requests, subprocess
 import pyautogui as pag
 from PIL import Image
 webhook_url = os.environ["WEBHOOK"]
+pag.sleep(1)
+img_filename = 'Image.png'
+pag.click(516, 405)
 subprocess.Popen(r'start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-pag.click(143, 487, duration=5)
+pag.sleep(1)
+pag.click(143, 487, duration=2)
 pag.click(155, 554, duration=2)
 pag.click(637, 417, duration=2)
 pag.click(588, 10, duration=2)
-time.sleep(2)
-img_filename = 'Image.png'
-pag.click(516, 405, duration=4)
-time.sleep(40)
-pag.click(249, 203, duration=4)
-time.sleep(1)
+while not pag.locateOnScreen('res/A-ready.png', confidence=0.9):
+    pag.sleep(1)
+pag.sleep(1)
+pag.click(249, 203)
+pag.sleep(1)
 pag.screenshot().save(img_filename)
 Image.open(img_filename).crop((230, 120, 500, 160)).save(img_filename)
 try:
