b9b3f83d9059ecc04e63cd7b59d00c27aaa892bd
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-18 15:01:47 +0300

    cut wait time

--- res/setup.py
+++ res/setup.py
@@ -7,14 +7,11 @@ actions = [
     (516, 405, 4),  # install (wait 15sec)
     (50, 100, 1),   # tic launch avica
     (249, 203, 4),  # allow rdp (attempt to activate the Allow button)
-    (249, 203, 4),  # allow rdp (attempt to activate the Allow button again)
-    (249, 203, 4),  # allow rdp (attempt to activate the Allow button again)
-    (249, 203, 4),  # allow rdp (attempt to activate the Allow button again)
     (447, 286, 4),  # ss id & upload (launch avica and take screenshot and send to gofile)
 ]
 
 # Give time to focus on the target application
-time.sleep(10)
+time.sleep(5)
 num = 0
 pag.screenshot().save(f"img{num}.png")
 num += 1
@@ -31,12 +28,7 @@ for x, y, duration in actions:
         pag.click(x, y, duration=duration)  # Try activating the button again
     pag.screenshot().save(f"img{num}.png")
     num += 1
-    
     if (x, y) == (447, 286):  # Launch avica and upload screenshot
-        os.system("C:/Program Files x86/Avica/Avica.exe")
-        time.sleep(5)  # Give some time for the app to launch
-        pag.click(249, 203, duration=4)  # Re-click on the Allow button coordinates
-        time.sleep(10)  # Extra 10 seconds delay before taking the screenshot
         pag.screenshot().save(img_filename)
         Image.open(img_filename).crop((230, 120, 500, 160)).save(img2)
         try:
