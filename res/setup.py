49a67de7d304fcb9520572a79ced63165f2df989
Noob <111202059+Noob-Lol@users.noreply.github.com>
2024-10-23 00:48:38 +0300

    Update setup.py

--- res/setup.py
+++ res/setup.py
@@ -1,6 +1,5 @@
-import os, requests, subprocess
+import os, requests, subprocess, re, pyperclip
 import pyautogui as pag
-from PIL import Image
 webhook_url = os.environ["WEBHOOK"]
 pag.sleep(1)
 img_filename = 'Image.png'
@@ -11,15 +10,25 @@ pag.click(143, 487, duration=2)
 pag.click(155, 554, duration=2)
 pag.click(637, 417, duration=2)
 pag.click(588, 10, duration=2)
+pag.sleep(1)
 while not pag.locateOnScreen('res/A-ready.png', confidence=0.9):
     pag.sleep(1)
-pag.sleep(1)
-pag.click(249, 203)
-pag.sleep(1)
-pag.screenshot().save(img_filename)
-Image.open(img_filename).crop((230, 120, 500, 160)).save(img_filename)
+pag.sleep(2)
+pag.click(249, 203, duration=2)
+pag.sleep(.5)
+pag.click(301, 105, duration=2)
+pag.sleep(.5)
+
+text = pyperclip.paste()
+match = re.search(r"Avica ID:\s*(\d{3} \d{3} \d{3}).*?Password:\s*([A-Za-z0-9]+)", text, re.DOTALL)
 try:
-    requests.post(webhook_url, files={"file": open(img_filename, "rb")})
+    if match:
+        requests.post(webhook_url, json={"content": f"ID: {match.group(1)}\nPass: {match.group(2)}"})
+    else:
+        print("Details not found, sending screenshot.")
+        pag.sleep(1)
+        pag.screenshot.save(img_filename)
+        requests.post(webhook_url, files={"file": open(img_filename, "rb")})
     print("Connection info was sent via webhook.")
 except Exception as e:
     print(f"An error occurred: {e}")
